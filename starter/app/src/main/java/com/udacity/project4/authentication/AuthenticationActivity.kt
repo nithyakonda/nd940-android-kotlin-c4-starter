62d3138c04fbd0664f0c3bc6e7be085347f98dd1
nithyakonda <nithyakonda1612@gmail.com>
2023-10-18 10:19:37 -0400

    Create and style map view
    requests permission and zooms to current location.

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,10 +1,14 @@
 package com.udacity.project4.authentication
 
+import android.Manifest
+import android.annotation.TargetApi
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -16,49 +20,55 @@ import com.udacity.project4.locationreminders.RemindersActivity
  * signed in users to the RemindersActivity.
  */
 class AuthenticationActivity : AppCompatActivity() {
+
+
     companion object {
-        const val TAG = "AuthenticationActivity"
-        const val SIGN_IN_RESULT_CODE = 1001
+        private const val TAG = "AuthenticationActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+        authenticateUser()
+
+
+        // TODO: a bonus is to customize the sign in flow to look nice using :
+        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    }
+
+
+
+    private fun authenticateUser() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
-        var activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val data: Intent? = result.data
-            val response = IdpResponse.fromResultIntent(data)
+        var activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                val data: Intent? = result.data
+                val response = IdpResponse.fromResultIntent(data)
 
-            if (result.resultCode == RESULT_OK) {
-                Log.i(
-                    TAG,
-                    "Successfully signed in user " +
-                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"+
-                            "provider id " + "${FirebaseAuth.getInstance().currentUser?.providerId}"
-                )
-                FirebaseAuth.getInstance().currentUser?.providerId
-                startActivity(Intent(this, RemindersActivity::class.java))
-            } else {
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+                if (result.resultCode == RESULT_OK) {
+                    Log.i(
+                        TAG,
+                        "Successfully signed in user " +
+                                "${FirebaseAuth.getInstance().currentUser?.displayName}!" +
+                                "provider id " + "${FirebaseAuth.getInstance().currentUser?.providerId}"
+                    )
+                    FirebaseAuth.getInstance().currentUser?.providerId
+                    startActivity(Intent(this, RemindersActivity::class.java))
+                } else {
+                    Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+                }
             }
-        }
         activityResultLauncher.launch(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .setIsSmartLockEnabled(false)
-                .build())
-
-
-
-        // TODO: If the user was authenticated, send him to RemindersActivity
-
-        // TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+                .build()
+        )
     }
 
 
--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,8 +1,30 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import com.udacity.project4.BuildConfig
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -10,11 +32,48 @@ import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
+
+    private val runningQOrLater = android.os.Build.VERSION.SDK_INT >=
+            android.os.Build.VERSION_CODES.Q
+    private var isZoomed = false
+    companion object {
+        private const val TAG =  "SelectLocationFragment"
+    }
 
     // Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+    private lateinit var map: GoogleMap
+    private lateinit var backgroundPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var foregroundPermissionLauncher: ActivityResultLauncher<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        foregroundPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                Log.d(TAG, "Show snack bar from foregroundPermissionLauncher")
+                showPermissionDeniedSnackBar()
+            } else {
+                // TODO Check if location permissions are granted and if so enable the
+                // location data layer.
+                Log.d(TAG, "Calling enableMyLocation from activity result")
+                enableMyLocation()
+                zoomToLastLocation()
+            }
+        }
+
+        if (runningQOrLater) {
+            backgroundPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (!isGranted) {
+                    Log.d(TAG, "Show snack bar from backgroundPermissionLauncer")
+                    showPermissionDeniedSnackBar()
+                } else {
+                    enableMyLocation()
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -24,19 +83,128 @@ class SelectLocationFragment : BaseFragment() {
 
         binding.viewModel = _viewModel
         binding.lifecycleOwner = this
-
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-        // TODO: add the map setup implementation
-        // TODO: zoom to the user location after taking his permission
-        // TODO: add style to the map
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        Log.d(TAG, "Calling enableMyLocation from onMapReady")
+        enableMyLocation()
+        zoomToLastLocation()
+        setMapStyle(map)
         // TODO: put a marker to location that the user selected
 
         // TODO: call this function after the user confirms on the selected location
         onLocationSelected()
-        return binding.root
     }
+
+
+
+    /** Permission Util Functions - Begin **/
+
+    private fun requestBackgroundPermission() {
+        if (runningQOrLater) {
+            Log.d(TAG, "requestBackgroundPermission")
+            backgroundPermissionLauncher.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+    }
+
+    private fun requestForegroundPermission() {
+        Log.d(TAG, "requestForegroundPermission")
+        foregroundPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+
+    private fun foregroundLocationPermissionApproved(): Boolean = (
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION) &&
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(requireContext(),
+                            Manifest.permission.ACCESS_COARSE_LOCATION))
+    @TargetApi(29)
+    private fun backgroundPermissionApproved()  {
+        if (runningQOrLater) {
+            PackageManager.PERMISSION_GRANTED ==
+                    ActivityCompat.checkSelfPermission(
+                        requireContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    )
+        } else {
+            true
+        }
+    }
+
+    /** Permission Util Functions - End **/
+
+
+    /** Map Manipulation functions - Begin **/
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (foregroundLocationPermissionApproved()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestForegroundPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun zoomToLastLocation() {
+        val zoomLevel = 15f
+        val location =
+            LocationServices.getFusedLocationProviderClient(requireActivity()).lastLocation
+        location.addOnSuccessListener {
+            it?.let {
+                val home = LatLng(it.latitude, it.longitude)
+                map.addMarker(MarkerOptions().position(home).title("Home"))
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, zoomLevel))
+                isZoomed = true
+            }
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireActivity(),
+                    R.raw.map_style
+                )
+            )
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        }catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
+    }
+
+    /** Map Manipulation functions - End **/
+
+
+    /** UI Utils - Begin **/
+    private fun showPermissionDeniedSnackBar() {
+        Snackbar.make(
+            binding.mapsContainer,
+            R.string.permission_denied_explanation,
+            Snackbar.LENGTH_INDEFINITE
+        )
+            .setAction(R.string.settings) {
+                startActivity(Intent().apply {
+                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                })
+            }.show()
+    }
+    /** UI Utils - End **/
 
     private fun onLocationSelected() {
         // TODO: When the user confirms on the selected location,
