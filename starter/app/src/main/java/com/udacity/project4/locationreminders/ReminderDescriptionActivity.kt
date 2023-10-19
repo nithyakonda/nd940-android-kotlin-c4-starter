c03ec947e7d7f1dd4d2a16caf3fec0524a91a4db
nithyakonda <nithyakonda1612@gmail.com>
2023-10-19 11:33:09 -0400

    Add UI for reminder description

--- starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
@@ -31,6 +32,6 @@ class ReminderDescriptionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val layoutId = R.layout.activity_reminder_description
         binding = DataBindingUtil.setContentView(this, layoutId)
-        // TODO: Add the implementation of the reminder details
+        binding.reminderDataItem = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem?
     }
 }
